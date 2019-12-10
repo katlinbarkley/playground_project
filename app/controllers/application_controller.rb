fc3b8ad09c3f985f2a29d6794f95b3d527ae2236
buriancl <burian.christopher@gmail.com>
2019-12-10 16:29:22 -0500

    added profile registration form fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio, :zip, :profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio, :zipcode, :profile])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :bio, :zip, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :bio, :zipcode, :profile])
   end
 end
 #test
