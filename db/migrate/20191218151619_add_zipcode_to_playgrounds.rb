e3b9e1fe8abcf9d87727be10b79abd1dc366a3d6
buriancl <burian.christopher@gmail.com>
2019-12-19 10:05:18 -0500

    added geocoder and form changes

--- db/migrate/20191218151619_add_zipcode_to_playgrounds.rb
+++ db/migrate/20191218151619_add_zipcode_to_playgrounds.rb
@@ -1,8 +1,8 @@
 class AddZipcodeToPlaygrounds < ActiveRecord::Migration[6.0]
   def change
     add_column :playgrounds, :zipcode, :string
-    add_column :playgrounds, :lat, :string
-    add_column :playgrounds, :long, :string
+    add_column :playgrounds, :latitude, :string
+    add_column :playgrounds, :longitude, :string
     add_column :playgrounds, :outdoor, :boolean
   end
 end
