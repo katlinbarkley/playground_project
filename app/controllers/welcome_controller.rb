f855130125b68a14c35d04741671b5d31f68dd68
Murat Yorukoglu <murat.yorukoglu@gmail.com>
2019-12-20 09:13:26 -0500

    updates to home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,25 @@
 class WelcomeController < ApplicationController
   def index
-    @playgrounds= Playground.all
     @zipcode = params[:zipcode]
     @lat = params[:lat]
     @long = params[:long]
-    @radius = params[:radius]
     @food = params[:food]
     @restrooms = params[:restrooms]
     @indoor = params[:indoor]
     @outdoor = params[:outdoor]
+    @radius = params[:radius]
+
+    @playgrounds = []
+
+    if @long.present? && @lat.present?
+      coord = [@lat,@long]
+      @radius = params[:radius].to_i
+      Playground.all.each do |playground|
+        if playground.distance_to(coord) <= @radius
+          @playgrounds << playground
+        end
+
+      end
+    end
   end
 end
